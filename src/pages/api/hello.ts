bdfe326a3c0dc6732352a8823d48a9e581191bda
fined000@naver.com <fined000@naver.com>
2023-05-25 22:35:14 +0900

    first commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import FirebaseAdmin from '../../../mode/firebase_admin'
 
 type Data = {
   name: string
@@ -9,5 +10,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  FirebaseAdmin.getInstance().Firebase.collection('test');
   res.status(200).json({ name: 'John Doe' })
 }
